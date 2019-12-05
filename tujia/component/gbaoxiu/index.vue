ba67bd83281b8b6c29dae7885c646ecd181e1c64
unknown <189363638@qq.com>
2019-12-05 14:26:51 +0800

    jy

---
+++ tujia/component/gbaoxiu/index.vue
@@ -0,0 +1,107 @@
+<template>
+	<view class="box">
+		<view>
+			<text>18754545</text>
+			<text>待处理</text>
+		</view>
+		<view>
+			<image src="../../static/logo.png" mode="aspectFill"></image>
+			<view>
+				<view>保修水管</view>
+				<view>地址</view>
+				<view>时间</view>
+			</view>
+		</view>
+		<view>
+			<view>查看详情</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		
+	}
+</script>
+
+<style lang='scss' scoped>
+	.box{
+		margin:29upx 35upx 0 35upx;
+		width:680upx;
+		height:315upx;
+		background:rgba(255,255,255,1);
+		box-shadow:0upx 1upx 9upx 0upx rgba(0, 0, 0, 0.16);
+		border-radius:10upx;
+		>view:nth-of-type(1){
+			height:60upx;
+			border-bottom:1upx solid #EEEEEE;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			padding:0 25upx;
+			text:nth-of-type(1){
+				font-size:24upx;
+				color:#333;
+			}
+			text:nth-of-type(2){
+				display: inline-block;
+				width:108upx;
+				height:40upx;
+				background:rgba(27,163,175,1);
+				font-size:24upx;
+				color:#fff;
+				text-align: center;
+				line-height:40upx;
+			}
+		}
+		>view:nth-of-type(3){
+			position:relative;
+			view{
+				width:141upx;
+				height:40upx;
+				border:1upx solid rgba(242,19,19,1);
+				border-radius:20upx;
+				line-height: 40upx;
+				text-align: center;
+				position:absolute;
+				top:10upx;
+				right:24upx;
+				font-size:24upx;
+				color:rgba(242,19,19,1);
+			}
+		}
+		>view:nth-of-type(2){
+			display: flex;
+			height:193upx;
+			align-items: center;
+			border-bottom:2upx solid #EEEEEE;
+			image{
+				width:217upx;
+				height:145upx;
+				margin:0 24upx;
+			}
+			>view{
+				display: flex;
+				flex-direction: column;
+				align-content: space-between;
+				justify-content: space-between;
+				height:145upx;
+				view:nth-of-type(1){
+					font-size:30upx;
+					font-weight:500;
+					color:rgba(51,51,51,1);
+				}
+				view:nth-of-type(2){
+					font-size:24upx;
+					font-weight:500;
+					color:rgba(102,102,102,1);
+				}
+				view:nth-of-type(3){
+					ont-size:18px;
+					font-weight:500;
+					color:rgba(153,153,153,1);
+				}
+			}
+		}
+	}
+</style>
---
+++ tujia/component/swiper/swiper.vue
@@ -0,0 +1,86 @@
+<template>
+	<view>
+		<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+			<swiper-item>
+				<view class="swiper-item uni-bg-red">A</view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item uni-bg-green">B</view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item uni-bg-blue">C</view>
+			</swiper-item>
+		</swiper>
+		<view class="nav1">
+			<view>
+				<view class="kuang orange">
+				<text class="cuIcon-barcode"></text>
+				</view>
+				<view class="content">街道管理</view>
+			</view>
+			<view>
+				<view class="kuang green">
+				<text class="cuIcon-friend"></text>
+				</view>
+				<view class="content">街道成员</view>
+			</view>
+			<view>
+				<view class="kuang blue">
+				<text class="cuIcon-settings"></text>
+				</view>
+				<view class="content">我的报修</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				 background: ['color1', 'color2', 'color3'],
+				 indicatorDots: true,
+				 autoplay: true,
+				 interval: 2000,
+				 duration: 500
+			}
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	.nav1{
+		display: flex;
+		height:200upx;
+		justify-content:space-around;
+		align-items: center;
+		>view{
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+		}
+	}
+	.kuang{
+		width:101upx;
+		height:101upx;
+		background:rgba(253,185,185,1);
+		border-radius:30upx;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text{
+			font-size:54upx;
+			color:#fff;
+		}
+	}
+	.content{
+		margin-top:8upx;
+	}
+	.green{
+		background:#14A4AF;
+	}
+	.blue{
+		background: #09417A;
+	}
+</style>
